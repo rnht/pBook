c9f309bf3ce107424cc12a8fb8272ab07c800fe6
rnht <rnht@kafkas-mac.local>
2014-05-29 21:12:37 -0400

    initial commit

---
+++ PBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace pBook
+{
+	public class PBook
+	{
+		public enum Error
+		{
+
+		}
+
+		private PBook ()
+		{
+		}
+
+		public static PBook create(Path file)
+		{
+
+		}
+
+		public static
+	}
+}
+
